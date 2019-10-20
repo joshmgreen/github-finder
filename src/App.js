0b0fabd6be8cb711caef5792dc0e6b1893a97d0d
Josh Green <joshx3g@gmail.com>
2019-10-19 23:39:35 -0500

    Lecture 32udemy

--- src/App.js
+++ src/App.js
@@ -1,51 +1,42 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './Components/Layout/Navbar';
-import Users from './Components/users/Users';
 import User from './Components/users/User';
-import Search from './Components/users/Search';
 import Alert from './Components/Layout/Alert';
 import About from './Components/pages/About';
 
 import GithubState from './Context/Github/GithubState';
+import AlertState from './Context/Alert/AlertState';
 
 import './App.css';
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
-
-  // Set Alert
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-
-    // Clear the alert message after a set amount of time
-    setTimeout(() => setAlert(null), 5000);
-  };
-
   return (
     <GithubState>
-      <Router>
-        <div className='App'>
-          <Navbar title='Github Finder' icon='fab fa-github' />
-          <div className='container'>
-            <Alert alert={alert} />
-            <Switch>
-              <Route
-                exact
-                path='/'
-                render={props => (
-                  <Fragment>
-                    <Search setAlert={showAlert} />
-                    <Users />
-                  </Fragment>
-                )}
-              />
-              <Route exact path='/about' component={About} />
-              <Route exact path='/user/:login' component={User} />
-            </Switch>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar title='Github Finder' icon='fab fa-github' />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route
+                  exact
+                  path='/'
+                  render={props => (
+                    <Fragment>
+                      <Search />
+                      <Users />
+                    </Fragment>
+                  )}
+                />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   );
 };
---
+++ src/Components/pages/Home.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+import Search from '../users/Search';
+import Users from '../users/User';
+
+const Home = () => {
+      <Search />
+      <Users />
+};
+
+export default Home
