b8e91e8a39a99d8b50127b59c5d8c9cabf16a83a
Josh Green <joshx3g@gmail.com>
2019-10-21 09:14:45 -0500

    Lecture 33 - Alert Context Workflow - fixed typoe in my reducer that prevented my Alert from showing

--- src/Context/Alert/alertReducer.js
+++ src/Context/Alert/alertReducer.js
@@ -3,7 +3,7 @@ import { SET_ALERT, REMOVE_ALERT } from '../types';
 export default (state, action) => {
   switch (action.type) {
     case SET_ALERT:
-      return action.pauload;
+      return action.payload;
     case REMOVE_ALERT:
       return null;
     default:
