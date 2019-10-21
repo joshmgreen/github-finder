44aed8015bb5bbc0881190faedb9db666bbf7753
Josh Green <joshx3g@gmail.com>
2019-10-21 09:55:20 -0500

    Lesson 35 - Prepare & Deploy To Netlify - Created a Home page and a Not found page to handle a user attempting to access an unavailable page. Created config to host on Netlify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './Components/Layout/Navbar';
 import User from './Components/users/User';
 import Alert from './Components/Layout/Alert';
 import About from './Components/pages/About';
 import Home from './Components/pages/Home';
+import NotFound from './Components/pages/NotFound';
 import GithubState from './Context/Github/GithubState';
 import AlertState from './Context/Alert/AlertState';
 
@@ -23,6 +24,7 @@ const App = () => {
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
